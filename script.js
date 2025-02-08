b24b7ae9ad4a60b6b36bd02fa55f67e0c6b576f9
chicadwell <chicadwell@gmail.com>
2025-02-09 00:59:42 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+document.querySelector('.yes-btn').addEventListener('click', function() {
+    document.querySelector('#yes-section').classList.remove('hidden');
+    document.querySelector('.container').classList.add('hidden');
+});
 
+document.querySelector('.no-btn').addEventListener('click', function() {
+    document.querySelector('#no-section').classList.remove('hidden');
+    document.querySelector('.container').classList.add('hidden');
+});
